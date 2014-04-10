3e4be053da73c313be9d52363a7f8ef9917b1585
bobbybee <j104@rocketmail.com>
2014-04-10 19:50:34 -0400

    Socket shell works two-way

--- webcat.js
+++ webcat.js
@@ -9,7 +9,7 @@ if(process.argv.length == 2) usage();
 if(process.argv[2] == '--help') help();
 
 function usage(e) {
-    console.log("node webcat.js [-lws] [ip] port");
+    console.log("node webcat.js [-lwsbq] [ip] port");
     if(!e) process.exit(0);
 }
 function help() {
@@ -17,6 +17,8 @@ function help() {
     console.log("-l: listen on port");
     console.log("-w: use websocket");
     console.log("-s: use socket.io");
+    console.log('-b: use binary data [unimplemented]')
+    console.log("-q: suppress all non-data")
     process.exit(0);
 }
 
@@ -26,6 +28,7 @@ var SOCKET = 0,
 
 var listen = false;
 var medium = SOCKET;
+var quiet = false;
 
 var ip = '0.0.0.0', port = 1234;
 
@@ -37,6 +40,7 @@ for(var i = 2; i < process.argv.length; ++i) {
             if(args[j] == 'l') listen = true;
             if(args[j] == 'w') medium = WEBSOCKET;
             if(args[j] == 's') medium = SOCKETIO;
+            if(args[j] == 'q') quiet = true;
         }
     } else if(i == process.argv.length - 2) {
         ip = arg;
@@ -45,4 +49,51 @@ for(var i = 2; i < process.argv.length; ++i) {
     }
 }
 
-console.log(ip+","+port+","+listen+","+medium);
+switch(medium) {
+case SOCKET:
+    var net = require('net');
+    if(listen) {
+        net.createServer(function(conn) {
+            newConn(conn);
+            conn.on('data', function(d) {
+                ondata(conn, d.toString());
+            });
+            conn.on('end', function(){ 
+                end(conn);
+            });
+        }).listen(port);
+    } else {
+        var sockcli = net.connect({port: port, host: ip}, function() {
+            newConn(sockcli)
+        });
+        sockcli.on('data', function(data) {
+            ondata(sockcli, data.toString())
+        })
+        sockcli.on('end', function(){
+            end(sockcli);
+        })
+    }
+    break;
+}
+
+var client = null;
+
+function newConn(conn) {
+    if(!quiet) console.log("open");
+    client = conn;
+}
+
+function ondata(conn, data) {
+    console.log(data);
+}
+
+function end(conn) {
+    process.exit(0);
+}
+
+process.stdin.resume();
+process.stdin.setEncoding("utf8");
+
+process.stdin.on("data", function(chunk) {
+    if(medium == SOCKET) client.write(chunk);
+})
