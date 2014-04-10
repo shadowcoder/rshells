31b4e7b04bd037cc027f5bcfa13010423e9b7e74
bobbybee <j104@rocketmail.com>
2014-04-10 19:36:49 -0400

    Options parsing

--- webcat.js
+++ webcat.js
@@ -19,3 +19,30 @@ function help() {
     console.log("-s: use socket.io");
     process.exit(0);
 }
+
+var SOCKET = 0,
+    WEBSOCKET = 1,
+    SOCKETIO = 2;
+
+var listen = false;
+var medium = SOCKET;
+
+var ip = '0.0.0.0', port = 1234;
+
+for(var i = 2; i < process.argv.length; ++i) {
+    var arg = process.argv[i];
+    if(arg[0] == '-') {
+        var args = arg.slice(1).split('');
+        for(var j = 0; j < args.length; j++) {
+            if(args[j] == 'l') listen = true;
+            if(args[j] == 'w') medium = WEBSOCKET;
+            if(args[j] == 's') medium = SOCKETIO;
+        }
+    } else if(i == process.argv.length - 2) {
+        ip = arg;
+    } else if(i == process.argv.length - 1) {
+        port = arg;
+    }
+}
+
+console.log(ip+","+port+","+listen+","+medium);
